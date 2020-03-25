067ca402f886c2b161bebd8651328c430a58c1b5
jordanclarkedev <62496066+jordanclarkedev@users.noreply.github.com>
2020-03-26 10:49:19 +1100

    Adding position info, restructured, more comments.

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ bot.on('ready', ()=>{    console.log("This bot is online");  });
 const token = process.env.API_KEY
     
 const roll= (msg)=>{
-
-
+    
+    //Parsing comment
     let msgString = msg;
-    let comment = "";
-
-    //Comment handling
+    let comment = "";    
     if(msg.includes('/')){
         msgString = msg.slice(0, msg.indexOf('/'));
         comment = "\n/ " + msg.slice( (msg.indexOf('/')+1) );   //Adds a little whitespace to the comment.       
@@ -20,33 +18,29 @@ const roll= (msg)=>{
 
     //Handling Error: No number given.
     if(     !/[0-9]/.test(msgString)    ){
-        return "Master, you've forgotten to specify what number to roll!\nYour humble servant beseeches you to ensure a number is before the comment ('/')."
+        return "You have my attention, but I'm not sure what to do.\nIf you don't know how I'm used, just say '/help'."
     }
 
-    let d = Number(/[0-9]+/.exec(msgString)[0]);
-    let r = /[rR]/.test(msgString);
-    let dice =  d || 2; //0d = roll 2 dice take lowest. Requires separate handling.
+    //Chucking all non-comment info into variables.
+    const d = Number(/[0-9]+/.exec(msgString)[0]);
+    const r = /[rR]/.test(msgString);
+    const dice =  d || 2; //0d = roll 2 dice take lowest. Requires separate handling.
 
-    
     //Organising the array of dice results.
     let array = []
     for(i=1;i<=dice;i++){
         array.push( Math.floor( (Math.random() *6) +1 ));
     }
-    array.sort().reverse();
+    array.sort((a, b) => b - a).reverse();
 
-    let num = d>0 ? array[0]: array[1]; //Take highest roll, or if 0d, take lowest of 2 rolls.
+    const num = d>0 ? array[0]: array[1]; //Take highest roll, or if 0d, take lowest of 2 rolls.
 
-
-    //Handling the reply message structure.
-    string = `${array.toString()}`;
-    if(comment){ string += comment; }
-
-
+    //Structing the string for replying.
+    const string = `${array.toString()}` + comment;
 
     if(r){ //Resistance roll handler
         if(d && array[1] === 6){
-            return `Critical! Recover 1 stress! \n${string}`;
+            return `Critical! Recover 1 stress!\n${string}`;
         } else {
             return `Take ${6-num} stress\n${string}`;
         }
@@ -60,36 +54,73 @@ const roll= (msg)=>{
                 return `Partial!\n${string}`;
             case (num <= 3):
                 return `Failure!\n${string}`;
-            }
         }
-
     }
+}
 
 
-
+const position = (pos) => {
+    switch(pos){
+        case "controlled":
+            return `Reduced Effect: -1 effect level
+            Worse Position: -1 position (can try again if failure)
+            Lost Opportunity: To try again, you'll need a new approach / action rating.
+            Complication: Immediate problem, 1 tick or +1 Heat.
+            Harm: Lesser Harm (level 1)`;
+            
+        case "risky":
+            return `Reduced Effect: -1 effect level
+            Worse Position: -1 position (can try again if failure)
+            Lost Opportunity: To try again, you'll need a new approach / action rating.
+            Complication: Immediate problem, 2 tick or +1 Heat.
+            Harm: Moderate Harm (level 2)`;
+            
+        case "desperate":
+            return `Reduced Effect: -1 effect level
+            Worse Position: -1 position (can try again if failure)
+            Lost Opportunity: To try again, you'll need a new approach / action rating.
+            Complication: Severe problem, 3 tick or +2 Heat.
+            Harm: Severe Harm (level 3)`;
+    }
+}
 
 
 
 bot.on("message", (msg)=>{
 
-    if(msg.content[0] === "!"){
-        msg.reply( roll(msg.content) );
-    }
+    //DO NOT ADD DEFAULT. The bot would respond to literally everything.
+    switch(true){
 
-    if(msg.content === "/help"){
-        msg.reply(`I am a bot for Blades in the Dark. Here's how to use me:
+        case (msg.content.toLowerCase() === "!controlled"):
+            msg.reply( position("controlled") );
+
+        case (msg.content.toLowerCase() === "!risky"):
+            msg.reply( position("risky") );
+
+        case (msg.content.toLowerCase() === "!desperate"):
+            msg.reply( position("desperate") );
+
+        //roll handling
+        case (msg.content[0]=== "!"):
+            msg.reply( roll(msg.content) );
+
+        case (msg.content.toLowerCase() === "/help"):
+            msg.reply(`I am a bot for Blades in the Dark. Here's how to use me:
         !                   Use ! to get its attention.
         ! 2                 The first number will make it roll that amount of d6s.
         ! 2 r               Including the letter 'r' will make it perform a resistance roll.
         ! r 2  / comment    Anything after the slash will be included as a comment.
 
-        Notes:
+        Note:
         ! resist 2d6        Works as you'd expect. The only things the bot looks for is the letter 'r' (not case-sensitive) and the first integer outside of a comment.
         
+        !controlled         Lists consequences for Controlled
+        !risky              Lists consequences for Risky actions
+        !desperate          Lists consequences for Desperate actions.
+
         /help               Makes me say this again.
         `);
     }
-
-})
+});
 
 bot.login(token);
